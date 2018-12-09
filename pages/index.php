b315753afa86d62388a401c26aefb9ef26baf988
clayton <me@clayton.cc>
2018-12-08 16:21:09 -0800

    Re-style
    Closes #3, Closes #5, Closes #6

--- pages/index.php
+++ pages/index.php
@@ -4,38 +4,46 @@
         <title>osu! Spotlights Team Application</title>
         <link rel="icon" type="image/png" href="https://s.ppy.sh/favicon-32x32.png" />
         <link rel="stylesheet" type="text/css" href="/style.css" />
+        <script>
+            function updateCounter(name, length) {
+                document.getElementById(name + "-counter").innerHTML = length;
+            }
+        </script>
     </head>
     <body>
         <div id="content">
-            <br/>
             <h1>osu! Spotlights Team Application</h1>
 
             <form method="post" action="/submit">
-                Game mode: <input name="mode" id="mode" placeholder="Choose one" list="modes" required/>
+                <div class="question">
+                    <span class="question__title">Game mode:</span>
+                    <input name="mode" placeholder="Choose one" list="modes" required/>
                     <datalist id="modes">
                         <option value="osu!">
                         <option value="osu!taiko">
                         <option value="osu!catch">
                         <option value="osu!mania">
                     </datalist>
-                <br/>
-                <br/>
-                    Discord tag: <input name="discord" id="discord" placeholder="placeholder#0000" pattern=".+#\d{4}" required/>
-                <br/>
-                <br/>
+                </div>
+
+                <div class="question question--grouped">
+                    <span class="question__title">Discord tag:</span>
+                    <input name="discord" placeholder="placeholder#0000" pattern=".+#\d{4}" required/>
+                </div>
 
                 <?php for ($i = 0; $i < count($questions); $i++): ?>
-                    <?= $questions[$i] ?>
-                        <br/>
-                        <br/>
-                            <textarea name="q<?= $i ?>" id="textarea" placeholder="Enter text here..." cols="50" rows="10" maxlength="1250" required></textarea>
-                        <br/>
-                        <br/>
-                    <?php endfor; ?>
+                    <div class="question">
+                        <span class="question__title"><?= $questions[$i] ?></span>
+                        <div class="textarea-wrapper">
+                            <textarea name="q<?= $i ?>" placeholder="Enter text here..." maxlength="1250" oninput="updateCounter(this.name, this.value.length)" required></textarea>
+                            <div class="counter"><span id="q<?= $i ?>-counter">0</span> / 1250</div>
+                        </div>
+                    </div>
+                <?php endfor; ?>
+
                 <div>
                     <input type="submit" class="button" value="Verify osu! account and send application"/>
                 </div>
-                <br/>
             </form>
         </div>
     </body>
