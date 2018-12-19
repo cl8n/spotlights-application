2d452a5d831b2c36879a1673f383cdc5d3ae00cf
clayton <me@clayton.cc>
2018-12-18 20:14:39 -0800

    Change page title

--- pages/submitted.php
+++ pages/submitted.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>osu! Spotlights Team Application</title>
+    <title>osu! Spotlights Team Application - Submitted</title>
     <link rel="icon" type="image/png" href="https://s.ppy.sh/favicon-32x32.png" />
     <link rel="stylesheet" type="text/css" href="/style.css" />
 </head>
 <body>
     <div id="content">
         <h1>osu! Spotlights Team Application</h1>
-        
+
         <p>Your application has been submitted! Make sure to <a href="<?= $config['discord']['invite'] ?>">join the osu! Spotlights Discord server</a> if you haven't already!</p>
     </div>
 </body>
